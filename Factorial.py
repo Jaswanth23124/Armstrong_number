4bc0e2fc35eb2dbd67836a716b7fe1f9a02ac667
Jaswanth23124 <jaswanthmalipeddi24@gmail.com>
2024-11-22 23:29:24 +0530

    Updated the code source

---
+++ Factorial.py
@@ -0,0 +1,27 @@
+import math
+def factorial_number(n):
+    result = math.factorial(n)
+    print(result)
+x = int(input("Enter a number to get its factorial number: "))
+factorial_number(x)
+
+
+'''
+import math
+n = int(input("Enter a number to get its Factorial Number: "))
+result = math.factorial(n)
+print(result)
+
+'''
+
+
+'''
+# Write a program to find its factorial number
+n = int(input("Enter a number to get its Factorial number: "))
+result = 1
+for i in range(n, 1,-1):
+    result *= i
+print(result)
+
+
+'''
